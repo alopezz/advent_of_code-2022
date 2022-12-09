7f0b18a53a31ca7d875b456ccee775cfe67d380c
Alex López <alex.lopez.zorzano@gmail.com>
2022-12-09 10:03:55 +0100

    Add executable to measure times

---
+++ day09/main.ml
@@ -0,0 +1,17 @@
+let time label f =
+  let t0 = Unix.gettimeofday () in
+  let result = f () in
+  Printf.printf "%s: %f\n" label (Unix.gettimeofday () -. t0);
+  result
+
+let main () =
+  let instructions = Day09.read_instructions stdin in
+  time "Time part 1"
+    (fun () -> Day09.solve_part1 instructions)
+  |> Printf.printf "Part 1: %d\n";
+  time "Time part 2"
+    (fun () -> Day09.solve_part1 instructions)
+  |> Printf.printf "Part 2: %d\n"
+
+
+let () = time "Total" main
